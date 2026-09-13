5df9af22a19abac08b7e7bea8c33562863a9f45e
VoltickVL <93241168+VoltickVL@users.noreply.github.com>
2026-09-13 11:09:42 +1000

    feat: register Settings DNS API

--- freenet-ui/settings_country_catalog.go
+++ freenet-ui/settings_country_catalog.go
@@ -26,6 +26,7 @@ type settingsCountryCatalogResponse struct {
 
 func registerSettingsCountryCatalogAPI(mux *http.ServeMux, a *app) {
 	mux.HandleFunc("GET /api/settings-v3/countries", a.requireAuth(a.handleSettingsCountryCatalog))
+	registerSettingsDNSAPI(mux, a)
 }
 
 func settingsCountryNameFromProfile(profile subscriptionProfile) string {
