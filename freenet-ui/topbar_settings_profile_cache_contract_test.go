ed59de52a91624fee7b933b6ec39407842c434df
VoltickVL <93241168+VoltickVL@users.noreply.github.com>
2026-09-19 19:08:39 +1000

    test(ui): scope cache contract to cache asset

--- freenet-ui/topbar_settings_profile_cache_contract_test.go
+++ freenet-ui/topbar_settings_profile_cache_contract_test.go
@@ -15,7 +15,6 @@ func TestTopbarSettingsProfileCacheContract(t *testing.T) {
 		"Используется последний успешный список Extra-профилей",
 		"currentProfilesWithCache",
 		"#fnVpnPickerToggle",
-		"fn-vpn-button-label",
 		"keepDialogInBody",
 		".fn-xray-topbar",
 		"height:50px!important",
